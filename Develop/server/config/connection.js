0de13339a81951faaf85c0c39e11b74b634e0f2d
kaivon tolooee <kaivont@gmail.com>
2021-08-05 16:41:25 -0400

    deploying to heroku

--- Develop/server/config/connection.js
+++ Develop/server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://kaivont:<Cbulls2323!>@cluster0.rdv47.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
